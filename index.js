5bc21a4d6bef0fa50fdcb3fca1c9c69522f5e936
truscaivoichita <sabo_voichita4sacrifice@yahoo.com>
2026-03-12 20:27:05 +0100

    modified so that I can receive the form input in my email address

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const toggle = document.getElementById("themeToggle");
+const form = document.getElementById("contactForm");
 
 function clickedImage() {
   alert("Image clicked");
 }
+
 function toggleTheme() {
   document.body.classList.toggle("dark");
 }
-toggle.addEventListener("click", toggleTheme);
+
+function handleFormSubmit(e) {
+  e.preventDefault();
+
+  emailjs
+    .send("service_c85al7k", "template_fyzox7a", {
+      name: form.name.value,
+      email: form.email.value,
+      message: form.message.value,
+    })
+    .then((response) => {
+      console.log("SUCCESS!", response.status, response.text);
+      alert("Email sent successfully!");
+      form.reset();
+    })
+    .catch((error) => {
+      console.error("FAILED...", error);
+      alert("Failed to send message.");
+    });
+}
+
+function initEvents() {
+  toggle.addEventListener("click", toggleTheme);
+  form.addEventListener("submit", handleFormSubmit);
+}
+
+initEvents();
